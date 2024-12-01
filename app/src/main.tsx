fc0fb58d93d6fcd1766f94a2253de5c6c863cad3
Ivan Mitrofanov <83586607+exodie@users.noreply.github.com>
2024-12-02 02:26:00 +0400

    feat: Initial ant-design UI to root (#2)

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,9 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from ".";
+import App from '.'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import { ConfigProvider, theme } from 'antd'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <ConfigProvider theme={{ ...theme, algorithm: theme.darkAlgorithm }}>
+      <App />
+    </ConfigProvider>
+  </StrictMode>,
+)
